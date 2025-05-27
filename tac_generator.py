576d37ce8e942d6a9d70a8cec20c391f6f20150b
PARAS SHARMA <145317890+ParasSharma101@users.noreply.github.com>
2025-05-27 23:59:35 +0530

    Implemented TAC generation logic to use temporary variables for expressions

--- tac_generator.py
+++ tac_generator.py
@@ -6,14 +6,40 @@ def new_temp():
     temp_counter += 1
     return temp
 
+def eval_expr(expr, code):
+   
+    if isinstance(expr, tuple):
+        op = expr[0]
+        left = expr[1]
+        right = expr[2]
+
+   
+        left_temp = eval_expr(left, code)
+        right_temp = eval_expr(right, code)
+
+        temp = new_temp()
+        code.append(f"{temp} = {left_temp} {op} {right_temp}")
+        return temp
+    else:
+        return expr
+
+ 
 def generate_TAC(ast):
     code = []
     for stmt in ast:
         if stmt[0] == 'assign':
-            temp = new_temp()
-            code.append(f"{temp} = {stmt[2]}")
-            code.append(f"{stmt[1]} = {temp}")
+            var_name = stmt[1]
+            expr = stmt[2]
+
+            result = eval_expr(expr, code)   
+            code.append(f"{var_name} = {result}")  
+
         elif stmt[0] == 'builtin_call':
-            func, args = stmt[1], stmt[2]
-            code.append(f"call {func}({', '.join(args)})")
+            func = stmt[1]
+            args = stmt[2]
+
+            
+            final_args = [eval_expr(arg, code) for arg in args]
+            code.append(f"call {func}({', '.join(final_args)})")
+
     return code
