611c2f47c3c98d49a638d17f6367513b50b5654c
thecoder-16 <natsupermacy@gmail.com>
2025-05-28 18:22:51 +0530

    updated parser.py

--- parser.py
+++ parser.py
@@ -1,32 +1,34 @@
-# parser.py
 
 def simple_parser(tokens):
     ast = []
     i = 0
 
+    print("Starting Syntax Analysis...\n")
+
     while i < len(tokens):
         token = tokens[i]
 
-        # Variable assignment: a = 10;
         if token[0] == 'ID' and i + 2 < len(tokens) and tokens[i+1][1] == '=':
-            var_name = tokens[i][1]
-            value = tokens[i+2][1]
-            ast.append(('assign', var_name, value))
-            i += 4  # Skip ; too
+            var = tokens[i][1]
+            val = tokens[i+2][1]
+            ast.append(('assign', var, val))
+            print(f"Parsed Assignment: {var} = {val}")
+            i += 4 
 
-        # Built-in function call: printf("x=%d", x);
         elif token[0] == 'BUILTIN_FUNC':
             func = token[1]
             args = []
-            i += 2  # skip function name and '('
+            i += 2  
             while tokens[i][1] != ')':
                 if tokens[i][0] not in ('SYMBOL', 'SKIP'):
                     args.append(tokens[i][1])
                 i += 1
             ast.append(('builtin_call', func, args))
-            i += 2  # Skip ')' and ';'
+            print(f"Parsed Function Call: {func} with args {args}")
+            i += 2  
 
         else:
             i += 1
 
+    print("\n AST (Abstract Syntax Tree) Created.\n")
     return ast
